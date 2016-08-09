148b6a337b2e01981f94c838cad06decc1b00c45
Isabel Tripp <stagestruck1@gmail.com>
2016-08-09 14:03:11 -0400

    accounts complies to first doctest/displays accnts, doctest incl. spacing

--- examples/accounts.py
+++ examples/accounts.py
@@ -1,8 +1,11 @@
 #demo account management program
 import argparse
 from nvm.fake_pmemobj import PersistentObjectPool, PersistentDict, PersistentList
+import decimal 
 #initial account creation module 
 
+#decimal precision
+decimal.getcontext().prec = 2
 
 #top-level parser
 parser = argparse.ArgumentParser()
@@ -10,18 +13,16 @@ parser.add_argument('--foo', action = 'store_true', help = 'foo help')
 subparsers = parser.add_subparsers(help = 'sub-command help', dest = 'subcommand')
 parser.add_argument('-f', '--filename', default='accounts.pmem', help="filename to store data in")
 
-#create the parser for the 'account create' command
+#create the parser for the 'accounts create' command
 parser_create = subparsers.add_parser('create', description= 'account creation')
 #(specify type of account)
 parser_create.add_argument('account', help = 'create specific type of bank account')
 #(establish initial balance in accnt)
-parser_create.add_argument('amount', help = "establish initial balance", type=float, default = 0, nargs = '?')
+parser_create.add_argument('amount', help = "establish initial balance", type=decimal.Decimal, default = decimal.Decimal('0.00'), nargs = '?')
 args_create = parser.parse_args()
 
 #temporary
 #print(args_create)
-
-#***use name of acct to create dictionary
 
 with PersistentObjectPool(args_create.filename, flag='c') as pop:
     if pop.root is None:
@@ -29,11 +30,22 @@ with PersistentObjectPool(args_create.filename, flag='c') as pop:
     accounts = pop.root
     if args_create.subcommand == 'create':
         accounts[args_create.account] = args_create.amount
-        print(accounts)
-    #if subcommand = 'transfer": 
+        print("Created account '" + args_create.account + "'.")
+    #if subcommand == 'transfer": 
     else:
         #check for accounts
-        #if ..
-        print()
-        print("No accounts currently exist.  Add an account using 'account create'.")
-    
+        if accounts:
+            m1= ("Account           Balance\n"
+                 "-------           -------")
+            print(m1)
+            accntTotal = 0
+            for specificAccount in accounts:
+                accntInfo = "{}             {}".format(specificAccount, accounts[specificAccount])
+                print(accntInfo)
+                accntTotal = accntTotal + accounts[specificAccount]
+            m2=("                         _______\n"
+                "    Net Worth:           {}").format(accntTotal)
+            print(m2)
+        #print message if no accounts exist
+        else:
+            print("No accounts currently exist.  Add an account using 'account create'.")
