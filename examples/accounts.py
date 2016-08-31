fdfb4ce8be5a9d5f78db380da5ed589b77877783
Isabel Tripp <stagestruck1@gmail.com>
2016-08-31 13:16:17 -0400

    withdraw and deposit function

--- examples/accounts.py
+++ examples/accounts.py
@@ -28,12 +28,22 @@ parser_create.add_argument('transferAmount', help ='quantity of money to transfe
 parser_create.add_argument('pastLocation', help ='account from which funds are withdrawn')
 parser_create.add_argument('futureLocation', help = 'account to which funds are deposited') 
 parser_create.add_argument('memo', help = 'explanation of transfer', nargs = argparse.REMAINDER)
-#withdraw money from account
+#withdraw money from account (check)
 parser_create = subparsers.add_parser('check', description = 'withdraw money via check')
 parser_create.add_argument('checkNumber', help = 'number of check')
 parser_create.add_argument('account', help = 'account from which to withdraw money')
 parser_create.add_argument('amount', help = 'amount withdrawn', type = decimal.Decimal, default = decimal.Decimal('0.00'))
 parser_create.add_argument('memo', help = 'check memo', nargs = argparse.REMAINDER)
+#deposit money into account
+parser_create = subparsers.add_parser('deposit', description = 'deposit money into account')
+parser_create.add_argument('account', help = 'account in which to deposit money')
+parser_create.add_argument('amount', help = 'total deposit', type = decimal.Decimal, default = decimal.Decimal('0.00'))
+parser_create.add_argument('memo', help = 'source of deposit', nargs = argparse.REMAINDER)
+#withdraw money from account (cash)
+parser_create = subparsers.add_parser('withdraw', description = 'withdraw amount of cash from account')
+parser_create.add_argument('account', help = 'account from which to withdraw money')
+parser_create.add_argument('amount', help = 'total withdrawl', type = decimal.Decimal, default = decimal.Decimal('0.00'))
+parser_create.add_argument('memo', help = 'reason for withdrawl', nargs = argparse.REMAINDER)
 args = parser.parse_args()
 
 
@@ -57,7 +67,8 @@ with PersistentObjectPool(args.filename, flag='c') as pop:
             accntBalance = accntBalance - transaction[1]
     elif args.subcommand == 'transfer':
         s= " "
-        memo = s.join(args.memo)                                                                        
+        memo = s.join(args.memo)
+        memo = memo[0].upper() + memo[1:]
         accounts[args.pastLocation].append(['2015-08-09', -decimal.Decimal(args.transferAmount), memo])         
         accounts[args.futureLocation].append(['2015-08-09', decimal.Decimal(args.transferAmount), memo])
         pBalance = decimal.Decimal(0)
@@ -70,11 +81,30 @@ with PersistentObjectPool(args.filename, flag='c') as pop:
     elif args.subcommand == 'check':
         c = " "
         memo = c.join(args.memo)
+        memo = memo[0].upper() + memo[1:]
         accounts[args.account].append(['2015-08-09', -decimal.Decimal(args.amount), "Check {}: {}".format(args.checkNumber, memo)])
         newBalance = decimal.Decimal(0)
         for transaction in accounts[args.account]:
             newBalance = newBalance + transaction[1]
         print("Check {} for {} debited from {} (new balance {})".format(args.checkNumber, args.amount, args.account, newBalance))
+    elif args.subcommand == 'deposit':
+        c = " "
+        memo = c.join(args.memo)
+        memo = memo[0].upper() + memo[1:]
+        accounts[args.account].append(['2015-08-09', decimal.Decimal(args.amount), memo])
+        newBalance = decimal.Decimal(0)
+        for transaction in accounts[args.account]:
+            newBalance = newBalance + transaction[1]
+        print("{} added to {} (new balance {})".format(args.amount, args.account, newBalance))
+    elif args.subcommand == 'withdraw':
+        c = " "
+        memo = c.join(args.memo)
+        memo = memo[0].upper() + memo[1:]
+        accounts[args.account].append(['2015-08-09', -decimal.Decimal(args.amount), memo])
+        newBalance = decimal.Decimal(0)
+        for transaction in accounts[args.account]:
+            newBalance = newBalance + transaction[1]
+        print("{} withdrawn from {} (new balance {})".format(args.amount, args.account, newBalance))
     else:
         #check for accounts
         if accounts:
